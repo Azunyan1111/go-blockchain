4e72a7cfd7066795d8fd0b5f948b8528f46d3029
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-07-22 15:23:58 +0900

    add main file. and add struct.

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "log"
+
+type Blockchain struct {
+
+}
+
+func (Blockchain) Init(){
+
+}
+
+func (Blockchain) NewBlock(){
+
+}
+
+func (Blockchain) NewTransaction(){
+
+}
+
+func (Blockchain) Hash(){
+
+}
+
+func (Blockchain) LastBlock(){
+
+}
+
+func main() {
+	log.Println("Hello Blockchain!")
+}
